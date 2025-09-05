aee63bac98580c8ae47b54c2d45f5ea703751ef9
Andrey <112519966+ahahaharu@users.noreply.github.com>
2025-09-05 15:52:25 +0300

    init

---
+++ WEB_353504_Bogdanovich.UI/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WEB_353504_Bogdanovich.UI.Models;
+
+namespace WEB_353504_Bogdanovich.UI.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
